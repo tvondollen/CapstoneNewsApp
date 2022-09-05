4208e52e14df310df6b24df1d202afee5d405081
tvondollen <Thomas.VonDollen@its.ny.gov>
2022-09-05 14:56:25 -0400

    Worked on W3 assignment, moved project out of main to week03

--- NewsApp/app/src/main/java/com/thomasvondollen/newsapp/Source.kt
+++ NewsApp/app/src/main/java/com/thomasvondollen/newsapp/Source.kt
@@ -1,10 +1,14 @@
 package com.thomasvondollen.newsapp
 
+import com.thomasvondollen.newsapp.models.Category
+import com.thomasvondollen.newsapp.models.Country
+import com.thomasvondollen.newsapp.models.Language
+
 data class Source(val id:String?, //nullable
-                  val name:String
+                  val name:String,
                   /*val description:String,
-                  val url:String,
-                  val category:String,
-                  val language:String,
-                  val country:String*/
+                  val url:String,*/
+                  val category: Category,
+                  val language: Language,
+                  val country:Country
                   )
